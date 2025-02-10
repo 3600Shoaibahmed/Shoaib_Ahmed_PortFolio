9a566f80ea380aa98643a32a55b739d6250a3456
Shoaib <shoaib8623803600@gmail.com>
2025-02-10 18:21:24 +0530

    Resume Updated

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/3600Shoaibahmed",
   resume:
-    "https://drive.google.com/file/d/1d4drifeHq2D8BqvlYrAvaTgelYRnAzJo/view?usp=sharing",
+    "https://drive.google.com/file/d/1hETRhD5uewgDw3eQg3zJ-zH5zFk6y2QB/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/shoaib-ahmed-ssa2712/",
   // twitter: "https://twitter.com/RishavChanda",
   insta: "https://www.instagram.com/shoaib_ahmed437?igsh=MTBwMjk2YnRyZGFrcw==",
