3100d22bcf33dc198bc7cca6fb0a515d1a1d2335
Shoaib <shoaib8623803600@gmail.com>
2025-02-11 14:21:46 +0530

    Minor changes in buttons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -103,7 +103,7 @@ function Footer() {
           <SocialMediaIcon href={Bio.insta} target="display"><InstagramIcon /></SocialMediaIcon>
         </SocialMediaIcons>
         <Copyright>
-          &copy; 2023 Shoaib Ahmed. All rights reserved.
+          &copy; 2025 Shoaib Ahmed. All rights reserved.
         </Copyright>
 
       </FooterWrapper>
